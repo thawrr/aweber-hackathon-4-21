8dfd46f77f9537758fc3adc91d70c52a7a453796
thawrr <thawrr@gmail.com>
2018-04-21 15:06:33 -0400

    uhhh

--- WebApplication1/admin.Master.designer.cs
+++ WebApplication1/admin.Master.designer.cs
@@ -38,14 +38,5 @@ namespace WebApplication1 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
-        /// ScriptManager1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
     }
 }
